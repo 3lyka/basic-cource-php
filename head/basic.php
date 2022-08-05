f517d808de6e57a8343c9b0c69bd618190f77050
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2022-08-05 21:47:41 +0300

    starting

---
+++ head/basic.php
@@ -0,0 +1,16 @@
+<?php
+/* Страница для констант */
+define('NAME_PAGE', 'Стартовая страница');
+//не совсем правильный метод, но так мы можем заложить кучу штук
+
+
+
+
+class Link
+{
+
+	const LINK_STYLE =  '<link rel="stylesheet" href="/style.css">';
+	const LINK_SCRIPT = '<script src="js/app.js"></script>';
+
+	const FAVICON = '<link href="/favicon.ico" type="/image/x-icon"/>';
+}
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+<?php require __DIR__ . '/head/basic.php'; ?>
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php print Link::FAVICON; ?>
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<!-- Подключение стилей -->
+	<?php print Link::LINK_STYLE; ?>
+	<!-- К примеру тут можно вывести яндекс метрику -->
+	<title> <?php print constant('NAME_PAGE') ?></title>
+</head>
+
+<body>
+	<div>
+		<h1>7 часовой курс, он надеюсь ок</h1>
+		<h2>2 часа посмотрел, вроде ок</h2>
+		<div class="flex">
+
+		</div>
+
+	</div>
+
+	<?php 
+
+
+
+
+
+	?>
+
+	<!-- Подключение скрипта -->
+	<?php print Link::LINK_SCRIPT; ?>
+</body>
+
+</html>
